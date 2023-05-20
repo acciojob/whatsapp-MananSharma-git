c8e1c5fb08167c242fa80db52d8ff8df08a1fcae
Manan <manansharma850@gmail.com>
2023-05-20 23:19:01 +0530

    check1

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -2,9 +2,50 @@ package com.driver;
 
 import java.util.Date;
 
-public class Message {
-    private int id;
+public class Message {private int id;
     private String content;
     private Date timestamp;
+
+    public Message() {
+    }
+
+    public Message(int id) {
+        this.id = id;
+    }
+
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 
 }
